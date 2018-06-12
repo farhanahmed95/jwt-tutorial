8a21fe7c4015599b13c9ab0173270189e8de330f
Farhan Ahmed <farhan.ahmed95@gmail.com>
2018-06-12 11:41:25 +0500

    no message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * JWT usage example
  * Steps
  * 1) Sign data with 30s expiry in our case is `user` object. look `login` route
- * 2) add middleware for checking if request have authorization tokens. look `ensureToken` function
+ * 2) add middleware for checking if request have authorization tokens. look `validateToken` function
  * 3) verify JWT and print data else set status code to 403 
  */
 
@@ -16,24 +16,18 @@ app.get("/",function(require,response){
 })
 app.post("/api/login",function(req,res)
 {
-    const user = {id:3}
+    const user = {id:1,username:'farhanahmed95'}
     const token = jwt.sign({user},'my_secret_key',{expiresIn:'30s'});
     res.json({token:token})
 })
-app.get("/api/protected",ensureToken,function(req,res)
+app.get("/api/user",validateToken,function(req,res)
 {
-    jwt.verify(req.token,'my_secret_key',function(err,data)
-{
-    
-    if(err)
-    {
-        res.sendStatus(403);
-    }else{
-        res.json({
-            text:'this is protected',
-            data:data
-        })
-    }
+    jwt.verify(req.token,'my_secret_key',function(err,data){
+        if(err){
+            res.sendStatus(403);
+        }else{
+            res.json(data);
+        }
 })
     
 })
@@ -41,7 +35,7 @@ app.listen(3000,function(){
     console.log("App listening on port 3000");
 })
 
-function ensureToken(req,res,next){
+function validateToken(req,res,next){
     const bearerHeader = req.headers["authorization"]
     if (typeof bearerHeader !== 'undefined')
     {
